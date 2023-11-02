eb5c4a8b379e9abf966fd61212ac336434c3adee
Felix Krause <felix_krause1999@yahoo.de>
2023-11-02 11:47:45 +0100

    linting most .py files

--- debug.py
+++ debug.py
@@ -20,6 +20,7 @@ import tensorflow_gan as tfgan
 import tqdm
 import io
 import inspect
+
 sns.set(font_scale=2)
 sns.set(style="whitegrid")
 
@@ -32,13 +33,14 @@ from models import layerspp
 from models import layers
 from models import normalization
 
-#from configs.ncsnpp import cifar10_continuous_ve as configs
+# from configs.ncsnpp import cifar10_continuous_ve as configs
 from configs.ddpm import cifar10_continuous_vp as configs
+
 config = configs.get_config()
 
 checkpoint = torch.load('exp/ddpm_continuous_vp.pth')
 
-#score_model = ncsnpp.NCSNpp(config)
+# score_model = ncsnpp.NCSNpp(config)
 score_model = ddpm_model.DDPM(config)
 score_model.load_state_dict(checkpoint)
 score_model = score_model.eval()
@@ -46,4 +48,4 @@ x = torch.ones(8, 3, 32, 32)
 y = torch.tensor([1] * 8)
 breakpoint()
 with torch.no_grad():
-  score = score_model(x, y)
+    score = score_model(x, y)
